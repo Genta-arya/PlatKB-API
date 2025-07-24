ec3a632a21b46e3436bf40ed0c155cc278cc2703
M.Gentha arya pratama <74033154+Genta-arya@users.noreply.github.com>
2025-07-24 17:46:12 +0700

    Update GajiController.js

--- controller/GajiController.js
+++ controller/GajiController.js
@@ -4,17 +4,17 @@ const prisma = new PrismaClient();
 
 export const getGajiTeknisi = async (req, res) => {
   try {
-    const allGaji = await prisma.gajiMekanik.findMany({
+  const allGaji = await prisma.gajiMekanik.findMany({
   where: {
-    mekanik: {
-      // hanya ambil jika relasi mekanik-nya ada
-      isNot: null,
+    mekanikId: {
+      not: null,
     },
   },
   include: {
     mekanik: true,
   },
 });
+
 
 
     if (!allGaji || allGaji.length === 0) {
